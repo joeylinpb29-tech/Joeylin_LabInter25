76f5485a96ef170d4a554a9f6722ba833aa4bbf6
joeylinpb29-tech <joeylinpb29@gmail.com>
2026-01-09 15:14:54 +0700

    Add files via upload
    ini yang final ya kak

--- LabPertama_withKakakNeolGanteng.py
+++ LabPertama_withKakakNeolGanteng.py
@@ -1,2 +1,22 @@
-letter = "z"
-print (letter)
+secret_letter = ["z", "Z"]
+
+while True:
+    input_letter = input("Ketik 1 huruf: ")
+    if input_letter in secret_letter:
+        print("Benar!")
+        break
+    elif len(input_letter) != 1 and input_letter.isalpha():
+        print("INVALID - Masukkan hanya 1 huruf!")
+        continue
+    elif len(input_letter) == 1 and not input_letter.isalpha():
+        print("INVALID - Masukkan huruf saja!")
+        continue
+    elif len(input_letter) != 1 and not input_letter.isalpha():
+        print("INVALID - Masukkan huruf saja dan hanya 1 huruf!")
+        continue
+    elif len(input_letter) == 1 and input_letter not in secret_letter:
+        print("Salah!")
+        break
+
+print("~Selesai~")
+
